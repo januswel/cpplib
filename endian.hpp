57011f9057ede8eeb1f26debfd54adbfd8324dbc
janus_wel <janus.wel.3@gmail.com>
2010-04-01 18:54:18 +0900

    Change the names of functions and refactoring
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

--- endian.hpp
+++ endian.hpp
@@ -17,33 +17,34 @@
 
 namespace util {
     namespace endian {
-        inline bool is_le() {
-            // in a 32-bit machine, the representation of 1 is:
-            //
-            //      little endian: 01 00 00 00
-            //      big endian:    00 00 00 01
-            //
-            // therefore the machine is little endian
-            // if the first byte is 1 (actually 01).
+        /*
+         *  In a 32-bit machine, the representation of 1 in type int is:
+         *
+         *      little endian: 01 00 00 00
+         *      big endian:    00 00 00 01
+         *
+         *  therefore the machine is little endian if the first byte is 1
+         *  (actually it's 0x01).
+         * */
+        inline bool is_little() {
             int t = 1;
             return *(util::cast::pointer_cast<char*>(&t)) == 1;
         }
-        inline bool is_be() { return !is_le(); }
+        inline bool is_big() { return !is_little(); }
+
+        // this is slow but nondestructive
+        template<typename T> T reverse(T value) {
+            char volatile* first = util::cast::pointer_cast<char volatile*>(&value);
+            char volatile* last = first + sizeof(T);
+            std::reverse(first, last);
+            return value;
+        }
 
         // this is destructive but fast
         template<typename T> void fast_reverse(T& value) {
             char volatile* first = util::cast::pointer_cast<char volatile*>(&value);
             char volatile* last = first + sizeof(T);
             std::reverse(first, last);
-        }
-
-        // this is slow but nondestructive
-        template<typename T> T reverse(const T& value) {
-            T temp = value;
-            char volatile* first = util::cast::pointer_cast<char volatile*>(&temp);
-            char volatile* last = first + sizeof(T);
-            std::reverse(first, last);
-            return temp;
         }
     }
 }
