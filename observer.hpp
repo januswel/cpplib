b58127964ed2146e68024f8de32b0bd3ed8a5591
janus_wel <janus.wel.3@gmail.com>
2010-04-04 16:21:26 +0900

    Add and fix some comments and fairing
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

--- observer.hpp
+++ observer.hpp
@@ -18,96 +18,118 @@ namespace pattern {
         template<typename T> class basic_observer;
 
         /*
-         * The class to change states
-         * To use, you must define the class or struct that is derived from this class.
+         *  a base class to change states
+         *  To use:
+         *
+         *      1. Define the class or struct that is derived from this class.
+         *      2. Define new member variables to indicates the state of the
+         *         object and new member functions to change states if those
+         *         variables aren't public.
+         *      3. Implement the member function state(0). This function must
+         *         return the value that is contained in the derived class as
+         *         member variables.
          * */
-        template<typename T>
-            class basic_subject {
-                public:
-                    // typedefs
-                    typedef T                       state_t;
-                    typedef basic_observer<state_t> observer_t;
+        template<typename T> class basic_subject {
+            public:
+                // typedefs
+                typedef T                       state_t;
+                typedef basic_subject<state_t>  this_t;
+                typedef basic_observer<state_t> observer_t;
 
-                    /* I think that std::list is best container to contain observers, because:
-                     *
-                     *  - The order of items that are added is kept, so the user of this class
-                     *    can handle the order of notifications.
-                     *      - For now, I exclude a sort of the list and sorted addition of a
-                     *        new items for simplicity.
-                     *  - There is a need for successive accesses to all items from start to
-                     *    end once, so it isn't the disadvantage that randome access is slow.
-                     *  - Adding and removing items are fast.
-                     *
-                     *  */
-                    typedef std::list<observer_t*>              observer_list_t;
-                    typedef typename observer_list_t::iterator  observer_it;
+                /*
+                 *  I think that std::list is best container to contain
+                 *  observers, because:
+                 *
+                 *      - The order of items that are added is kept, so the
+                 *        user of this class can handle the order of
+                 *        notifications.
+                 *          - For now, I exclude a sort of the list and sorted
+                 *            addition of a new items for simplicity.
+                 *      - There is a need for successive accesses to all items
+                 *        from start to end once, so it isn't the disadvantage
+                 *        that randome access is slow.
+                 *      - Adding and removing items are fast.
+                 *  */
+                typedef std::list<observer_t*>              observer_array_t;
+                typedef typename observer_array_t::iterator observer_array_it_t;
 
-                protected:
-                    // the list of observers
-                    observer_list_t observers;
+            protected:
+                // a list of observers
+                observer_array_t observers;
 
-                protected:
-                    // the member function to get current (latest) states
-                    virtual const state_t& state(void) const = 0;
+            public:
+                // typical destructor
+                virtual ~basic_subject(void) {}
 
-                public:
-                    // register an observer
-                    inline basic_subject& attach(observer_t& o) {
-                        observers.push_back(&o);
-                        return *this;
+                // register an observer
+                inline this_t& attach(observer_t& o) {
+                    observers.push_back(&o);
+                    return *this;
+                }
+
+                inline this_t& attach(observer_t* o) {
+                    observers.push_back(o);
+                    return *this;
+                }
+
+                // release an observer
+                inline this_t& detach(observer_t& o) {
+                    observers.remove(&o);
+                    return *this;
+                }
+
+                inline this_t& detach(observer_t* o) {
+                    observers.remove(o);
+                    return *this;
+                }
+
+                // notice the current states to all of observers
+                // Should we use std::for_each(3) ?
+                void notify(void) {
+                    state_t s = state();
+                    for (observer_array_it_t it = observers.begin();
+                            it != observers.end();
+                            ++it) {
+                        (*it)->update(s);
                     }
+                }
 
-                    inline basic_subject& attach(observer_t* o) {
-                        observers.push_back(o);
-                        return *this;
-                    }
-
-                    // release an observer
-                    inline basic_subject& detach(observer_t& o) {
-                        observers.remove(&o);
-                        return *this;
-                    }
-
-                    inline basic_subject& detach(observer_t* o) {
-                        observers.remove(o);
-                        return *this;
-                    }
-
-                    // notice the current states to all of observers
-                    // Should we use std::for_each(3) ?
-                    void notify(void) {
-                        state_t s = state();
-                        for (observer_it it = observers.begin(); it != observers.end(); ++it) {
-                            (*it)->update(s);
-                        }
-                    }
-
-                    // typical destructor
-                    virtual ~basic_subject(void) {}
-            };
+            protected:
+                // the member function to get current (latest) states
+                virtual const state_t& state(void) const = 0;
+        };
 
         /*
-         * The class to observe children of basic_subject class
-         * To use, you must define the class or struct that is derived from this class.
+         *  a base class to observe children of basic_subject class
+         *  To use:
+         *
+         *      1. Define the class or struct that is derived from this class.
+         *      2. Implement the member function handle(1).
          * */
-        template<typename T>
-            class basic_observer {
-                public:
-                    // typedefs
-                    typedef T state_t;
+        template<typename T> class basic_observer {
+            public:
+                // typedefs
+                typedef T                       state_t;
+                typedef basic_observer<state_t> this_t;
 
-                public:
-                    // Objects of this class are identified by the memory address of its instance.
-                    inline bool operator==(const basic_observer& rhs) const { return this == &rhs; }
-                    inline bool operator!=(const basic_observer& rhs) const { return !(*this == rhs); }
+            public:
+                // typical destructor
+                virtual ~basic_observer(void) {}
 
-                    // typical destructor
-                    virtual ~basic_observer(void) {}
+                // Objects of this class are identified by the memory
+                // address of its instance.
+                inline bool operator==(const this_t& rhs) const {
+                    return this == &rhs;
+                }
+                inline bool operator!=(const this_t& rhs) const {
+                    return !(*this == rhs);
+                }
 
-                public:
-                    // virtual functions
-                    virtual void update(const state_t& s) = 0;
-            };
+            public:
+                // a virtual function which should be implemented by
+                // derived classes
+                virtual void update(const state_t& s) = 0;
+        };
     }
 }
 
