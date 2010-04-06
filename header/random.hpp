eda52cf318e7cd52aa1742f140a0422c1dd0afc5
janus_wel <janus.wel.3@gmail.com>
2010-04-06 16:33:56 +0900

    Add
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

---
+++ header/random.hpp
@@ -0,0 +1,43 @@
+/*
+ * random.hpp
+ *  a class to generate pseudo-random floating point numbers (double)
+ *
+ *  written by janus_wel<janus.wel.3@gmail.com>
+ *  This source code is in public domain, and has NO WARRANTY.
+ * */
+
+#ifndef RANDOM_HPP
+#define RANDOM_HPP
+
+#include <cstdlib>  // for std::srand(1) and std::rand(0)
+#include <ctime>    // for std::time(1)
+
+namespace util {
+    namespace math {
+        class random {
+            public:
+                // constructor
+                random(void) {
+                    static bool is_initialized = false;
+                    if (!is_initialized) {
+                        std::srand(static_cast<unsigned int>(std::time(NULL)));
+                        is_initialized = true;
+                    }
+
+#ifdef _MSC_VER
+                    // discard first one to get better value
+                    // for Visual C++
+                    std::rand();
+#endif
+                }
+
+                // getter
+                double operator()(void) {
+                    return static_cast<double>(std::rand()) / RAND_MAX;
+                }
+        };
+    }
+}
+
+#endif // RANDOM_HPP
+
