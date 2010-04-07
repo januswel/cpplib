99af149b7a91f472161a210391161356a4bf71b8
janus_wel <janus.wel.3@gmail.com>
2010-04-07 18:20:23 +0900

    Add
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

---
+++ header/cor.hpp
@@ -0,0 +1,212 @@
+/*
+ * cor.hpp
+ *  the base classes for Chain of Responsibility pattern of GoF design patterns
+ *
+ *  written by janus_wel<janus.wel.3@gmail.com>
+ *  This source code is in public domain, and has NO WARRANTY.
+ * */
+
+#ifndef COR_HPP
+#define COR_HPP
+
+#include <list>
+#include <string>
+#include <stdexcept>
+
+namespace pattern {
+    namespace cor {
+        /*
+         *  a base class to handle data
+         *  The objects of the class types that are sub classes of this make a
+         *  chain.  To use:
+         *
+         *      1. Define a class or struct that is derived from this class.
+         *      2. Override the following member functions
+         *
+         *          - is_in_charge(const data_t&)
+         *              - must return true if the sub class has the
+         *                responsibility to handle the passed data.
+         *              - otherwise false.
+         *          - is_need_data(void)
+         *              - must return true if the sub class needs data to
+         *                handle responsibility.
+         *              - otherwise false.
+         *          - handle_responsibility(const data_t&)
+         *              - should be overridden if is_need_data(void) returns
+         *                true.
+         *          - handle_responsibility(void)
+         *              - should be overridden if is_need_data(void) returns
+         *                false.
+         *
+         *      3. Create objects of the sub classes and enlink(1) by
+         *         basic_chain.
+         *  */
+        template<typename retT, typename dataT> class basic_handler {
+            public:
+                // typedefs
+                typedef retT    return_t;
+                typedef dataT   data_t;
+
+            public:
+                // destructor
+                virtual ~basic_handler(void) {}
+
+                // This must return true if the data can be handled by the
+                // class.  Otherwise false.
+                virtual bool is_in_charge(const data_t&) const = 0;
+
+                // the choice to use handle_responsibility(const data_t&) or
+                // handle_responsibility(void)
+                virtual bool is_need_data(void) const = 0;
+
+                // the class basic_chain use this if is_need_data(void) is true
+                // the procedure to handle the data.
+                virtual return_t handle_responsibility(const data_t&) {
+                    throw std::logic_error("Override pattern::cor::basic_handler::handle_responsibility(const data_t&).");
+                }
+                // the class basic_chain use this if is_need_data(void) is false
+                // use this member function when you don't need data
+                virtual return_t handle_responsibility(void) {
+                    throw std::logic_error("Override pattern::cor::basic_handler::handle_responsibility(void).");
+                }
+        };
+
+        /*
+         *  a base class to handle a chain
+         *  To use:
+         *
+         *      1A. Define a class or struct that is derived from this class.
+         *          And override the member functions
+         *
+         *          - at_end_of_chain(void)
+         *              - is run when the specified data isn't handled by any
+         *                sub classes of basic_handler.
+         *              - Override if you want to change the behavior of this
+         *                function.
+         *
+         *      1B. Or, only instantiate by specifying template parameters.
+         *          Overmore give it handy name by using typedef.
+         *
+         *      2. Create objects of the sub or instantiated class and
+         *         enlink(1) objects of the sub classes of basic_handler, then
+         *         execute request(const data_t&).
+         *  */
+        template<typename retT, typename dataT> class basic_chain {
+            public:
+                typedef retT                                return_t;
+                typedef dataT                               data_t;
+                typedef basic_handler<return_t, data_t>     handler_t;
+
+            protected:
+                typedef handler_t*                          handler_ptr_t;
+                typedef std::list<handler_ptr_t>            handler_array_t;
+                typedef typename handler_array_t::iterator  handler_array_iterator;
+
+            private:
+                typedef basic_chain<return_t, data_t>       this_t;
+
+            public:
+                // destructor
+                virtual ~basic_chain(void) {}
+
+            protected:
+                // member variables
+                handler_array_t chain;
+
+            protected:
+                // the function to be run at the end of the chain
+                // Override this if you need other processings.
+                virtual return_t at_end_of_chain(void) {
+                    throw std::logic_error("The requested data has reached the end of the chain.");
+                }
+
+            public:
+                // enlink objects
+                this_t& enlink_chain(handler_t& h) {
+                    chain.push_back(&h);
+                    return *this;
+                }
+
+                this_t& enlink_chain(handler_t* h) {
+                    chain.push_back(h);
+                    return *this;
+                }
+
+                // the function to follow the chain
+                return_t request_to_chain(const data_t& data) {
+                    for (handler_array_iterator it = chain.begin();
+                            it != chain.end();
+                            ++it) {
+                        const handler_ptr_t& item = *it;
+                        if (item->is_in_charge(data)) {
+                            return item->is_need_data()
+                                ? item->handle_responsibility(data)
+                                : item->handle_responsibility();
+                        }
+                    }
+                    return at_end_of_chain();
+                }
+        };
+
+        // the partial specialization where retT = void
+        template<typename dataT> class basic_chain<void, dataT> {
+            public:
+                typedef void                                return_t;
+                typedef dataT                               data_t;
+                typedef basic_handler<void, data_t>         handler_t;
+
+            protected:
+                typedef handler_t*                          handler_ptr_t;
+                typedef std::list<handler_ptr_t>            handler_array_t;
+                typedef typename handler_array_t::iterator  handler_array_iterator;
+
+            private:
+                typedef basic_chain<void, data_t>           this_t;
+
+            public:
+                // destructor
+                virtual ~basic_chain(void) {}
+
+            protected:
+                // member variables
+                handler_array_t chain;
+
+            protected:
+                // the function to be run at the end of the chain
+                // Override this if you need other processings.
+                virtual void at_end_of_chain(void) {
+                    throw std::logic_error("The requested data has reached the end of the chain.");
+                }
+
+            public:
+                // enlink objects
+                this_t& enlink_chain(handler_t& h) {
+                    chain.push_back(&h);
+                    return *this;
+                }
+
+                this_t& enlink_chain(handler_t* h) {
+                    chain.push_back(h);
+                    return *this;
+                }
+
+                // the function to follow the chain
+                void request_to_chain(const data_t& data) {
+                    for (handler_array_iterator it = chain.begin();
+                            it != chain.end();
+                            ++it) {
+                        const handler_ptr_t& item = *it;
+                        if (item->is_in_charge(data)) {
+                            item->is_need_data()
+                                ? item->handle_responsibility(data)
+                                : item->handle_responsibility();
+                        }
+                    }
+                    at_end_of_chain();
+                }
+        };
+    }
+}
+
+#endif // COR_HPP
+
---
+++ sample/cor/main.cpp
@@ -0,0 +1,91 @@
+/*
+ * main.cpp
+ *  sample codes of cor.hpp
+ *
+ *  written by janus_wel<janus.wel.3@gmail.com>
+ *  This source code is in public domain, and has NO WARRANTY.
+ * */
+
+#include "../../header/cor.hpp"
+#include "../../header/dlogger.hpp"
+#include "../../header/tconv.hpp"
+#include <iostream>
+
+// TODO: use reference counting pointer for pointers to objects of PrimeNumber
+
+// a class that represents a prime number
+class PrimeNumber : public pattern::cor::basic_handler<bool, unsigned int> {
+    private:
+        const unsigned int value;
+
+    public:
+        // constructor
+        PrimeNumber(unsigned int value) : value(value) {
+            DBGLOG("PrimeNumber(" << value << ")");
+        }
+        // destructor
+        ~PrimeNumber(void) {
+            DBGLOG("~PrimeNumber(" << value << ")");
+        }
+        // assignment operator isn't defined
+        PrimeNumber& operator=(const PrimeNumber&);
+
+        // implementations of virtual functions
+        bool is_in_charge(const unsigned int& n) const {
+            return (n % value) == 0;
+        }
+        bool is_need_data(void) const { return false; }
+        bool handle_responsibility(void) {
+            DBGLOG("handle(" << value << ")");
+            return false;
+        }
+};
+
+// a sequence of prime numbers
+class PrimeNumbers : public pattern::cor::basic_chain<bool, unsigned int> {
+    public:
+        // constructor
+        PrimeNumbers(void) {
+            DBGLOG("PrimeNumbers()");
+        }
+        // destructor
+        ~PrimeNumbers(void) {
+            DBGLOG("~PrimeNumbers()");
+            // TODO
+            for (handler_array_iterator it = chain.begin(); it != chain.end(); ++it) {
+                delete *it;
+            }
+        }
+        // assignment operator isn't defined
+        PrimeNumbers& operator=(const PrimeNumbers&);
+
+        // check whether a specified number is prime number or not
+        bool is_prime_number(const unsigned int n) { return request_to_chain(n); }
+        // add a new prime number
+        // TODO
+        void add(unsigned int n) { enlink_chain(new PrimeNumber(n)); }
+
+    protected:
+        // implementations of virtual functions
+        bool at_end_of_chain(void) { return true; }
+};
+
+int main(const int argc, const char* const argv[]) {
+    // a max of calculation range
+    util::string::tconv tconv;
+    unsigned int max = (argc < 2)
+        ? 100
+        : tconv.strto<unsigned int>(argv[1]);
+
+    // pick up prime numbers
+    PrimeNumbers pn;
+    for (unsigned int n = 2; n < max; ++n) {
+        if (pn.is_prime_number(n)) {
+            std::cout << n << std::endl;
+            pn.add(n);
+        }
+    }
+
+    return 0;
+}
+
