c4d71f63835c02243fb8bd723787659b35dbaa5a
janus_wel <janus.wel.3@gmail.com>
2010-04-30 18:35:21 +0900

    Refactoring to use util::string::print_op
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

--- sample/nwconv/main.cpp
+++ sample/nwconv/main.cpp
@@ -9,25 +9,29 @@
 #include <iostream>
 #include <string>
 
+#include "../../header/algorithm.hpp"
 #include "../../header/nwconv.hpp"
 #include "../../header/wexcept.hpp"
 
 using namespace std;
+using namespace util::algorithm;
 using namespace util::string;
 using namespace util::exception;
 
+string second_or_later(const char* const src) {
+    static nwconv conv;
+
+    // convert an argument narrow to wide
+    wstring arg = conv.ntow(src);
+    // discard a first character
+    arg.erase(0, 1);
+    // convert it wide to narrow and print
+    return (arg.size() > 1) ? conv.wton(arg) : string("");
+}
+
 int main(const int argc, const char* const argv[]) {
     try {
-        nwconv conv;
-
-        for (int i = 0; i < argc; ++i) {
-            // convert an argument narrow to wide
-            wstring arg = conv.ntow(argv[i]);
-            // discard a first character
-            arg.erase(0, 1);
-            // convert it wide to narrow and print
-            cout << conv.wton(arg) << endl;
-        }
+        print_op<string>(argv, argv + argc, std::cout, "\n", second_or_later);
     }
     catch (exception& ex) {
         // for nwconv::ntow(1)
