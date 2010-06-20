8b71adcdee4e18b0a9d31501fe5537d0b0f6a5e0
janus_wel <janus.wel.3@gmail.com>
2010-06-20 17:12:06 +0900

    Add
    forgotten one
    
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

---
+++ framework/skeleton/global.cpp
@@ -0,0 +1,13 @@
+/*
+ * global.cpp
+ *  Definitions for global objects
+ *
+ *  written by janus_wel<janus.wel.3@gmail.com>
+ *  This source code is in public domain, and has NO WARRANTY.
+ * */
+
+#include "sample.hpp"
+#include <locale>
+
+util::string::typeconverter tconv(std::locale::classic());
+
